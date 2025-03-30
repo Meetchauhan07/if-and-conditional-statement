4451343879e7039c1eb553296f36d43ce9665c18
Meetchauhan07 <meet.chauhan.mk@gmail.com>
2025-03-30 15:21:32 +0530

    Add files via upload

---
+++ odd or even,divisible by 10,minor or Major.py
@@ -0,0 +1,37 @@
+#even and odd number
+
+num=int(input("Enter a number:"))
+if num % 2 == 0:
+    print(num," is even.")
+else:
+    print(num," is odd.")
+'''
+output:
+Enter a number:15
+15  is odd.
+'''
+#Divisible by 10
+num=int(input("Enter a number:"))
+if num%10==0:
+    print(num," is divisible by 10.")
+else:
+    print(num," is not divisible by 10.")
+'''
+output:
+Enter a number:50
+50  is divisible by 10.
+'''
+    
+#major and minor
+age=int(input("Enter your age: "))
+if age<18:
+    print("Minor")
+else:
+    print("Major")
+'''
+output:
+Enter your age: 53
+Major
+'''
+
+    
